46643b89124a1c0e3a0a81a567b65cc258039337
quincyDaVinci <quincyboschman@outlook.com>
2026-05-27 16:02:34 +0200

    fix: update import from CalibrationOverlay.scss to CalibrationOverlay.css

--- src/components/mirror/CalibrationOverlay.tsx
+++ src/components/mirror/CalibrationOverlay.tsx
@@ -1,5 +1,5 @@
 import type { MirrorSettings } from "../../types/settings";
-import "./CalibrationOverlay.scss";
+import "./CalibrationOverlay.css";
 
 type CalibrationOverlayProps = {
   settings: MirrorSettings;
